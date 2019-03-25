effaa73d0f0ff7e881b971e8c5e51bac8160c307
GennadyC <gena_chursin1995@mail.ru>
2019-03-25 19:06:48 +0700

    Update start_server.py

--- start_server.py
+++ start_server.py
@@ -3,24 +3,27 @@ import socket
 import time
 from multiprocessing import Process, Queue
 import select
-import socket
 import sys
 import requests
 import logging
 import json
+import queue
+
 
 q=Queue()
 
 def post_send(msg):
-    res = requests.post('http://127.0.0.1:8200/api/services/device_key_service/rebroadcast', json={"tx_body":msg})
+    try:
+        #res = requests.post('http://127.0.0.1:8200/api/services/device_key_service/rebroadcast', json={"tx_body":msg})
+        res = requests.post('http://192.168.88.112:8200/api/services/device_key_service/rebroadcast', json={"tx_body":msg})
+    
+    except Exception:
+        pass 
 
 def post_send_subscribe(msg):
     res = requests.post('http://127.0.0.1:8100/api/services/device_key_service/subscribe', json={"tx_body":msg})
 
-#def post_send_register(msg):
-#    res = requests.post('http://127.0.0.1:8200/api/services/device_key_service/unsubscribe', json={"tx_body":msg})
-
-def get_send():
+def get_wearout():
     res = requests.get('http://127.0.0.1:8200/api/services/device_key_service/get_short_device_key?public_key=91fc61fc1ed1856da51725f530951917ab5301d1c1f72a88a86c2950cb8f44ff')
     if res.ok:
         json_data = json.loads(res.text)
@@ -28,15 +31,6 @@ def get_send():
         return s
     else:
         return 1111111
-
-def get_send_register():
-    res = requests.get('http://127.0.0.1:8100/api/services/device_key_service/get_subscriber?public_key=91fc61fc1ed1856da51725f530951917ab5301d1c1f72a88a86c2950cb8f44ff')
-    if (res.text == 'Subscriber PublicKey(91fc61fc...) not found'):
-        result = 'no'
-        return result
-    else:
-        result = 'yes'
-        return result
 
 
 class MyServer(ApiServer):
@@ -55,66 +49,62 @@ class MyServer(ApiServer):
         return {"obj":1}
 
 def sock(q):
-    timeout = 10.0
-    s = socket.socket()
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind(('0.0.0.0', 5001 ))
-    #s.setblocking(0)
-    s.listen(0)
-    addr=False
-    while True:
-        cli = False
-        try:
-            client, addr = s.accept()
-        except socket.error:
-            continue
-        client.settimeout(timeout)
-        try:
-            content = client.recv(1024)
-        except socket.error:
-            continue
-        sub=False
-        if (content.decode("utf-8") == 'Subscribe'):
-            sub = get_send_register()
-            if sub == 'yes':
-                client.send(str(sub).encode())
-            if sub == 'no':
-                client.send(str(sub).encode())
-                try:
-                   content = client.recv(1024)
-                except socket.error:
-                    continue
-                post_send_subscribe(content.decode("utf-8"))
-        
-        status = False
-        try:
-            content = client.recv(1024)
-        except socket.error:
-            continue
-        if (content.decode("utf-8") == 'Register'):
-            status = get_send()
-        client.send(str(status).encode())
-        try:
-            content = client.recv(1024)
-            post_send(content.decode("utf-8"))
-            cli = True
-        except Exception:
-            cli = False
-        while cli:
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(('0.0.0.0', 5001 ))
+    server.setblocking(0)
+    server.listen(1024)
+    addr = False
+    inputs = [server] #Сокеты которые планируем читать
+    outputs = []      #Сокеты в которые планируем писать
+    message_queues = {}
+    public_key_socket = {}
+
+    while inputs:
+        readable, writable, exceptional = select.select (inputs, outputs, inputs)
+        #ЧТЕНИЕ
+        for s in readable:
+            if s is server:
+                connection, client_address = s.accept()
+                print(client_address)
+                connection.setblocking(0)
+                inputs.append(connection)
+                outputs.append(connection)
+                message_queues[connection] = queue.Queue()
+
+            else:
+                data = s.recv(1024)
+                if data:
+                    if data[0:7] == "404Code":
+                        post_send_subscribe(data[7:])
+                        public_key_socket[data[7:64]] = s
+                        s.send(get_wearout(data[7:64]))
+                    if data[0:7] == "410Code":
+                        post_send(data[7:])
+                else:
+                    if s in outputs:
+                        outputs.remove(s)
+                    inputs.remove(s)
+                    s.close()
+                    del message_queues[s]
+        #ЗАПИСЬ
+        uslovie = True
+        while uslovie:
+            data = ""
             try:
-                k=q.get(timeout=5)
-                if k:
-                    client.send(k.encode())
-                    k=0
-                    try:
-                        content = client.recv(1024)
-                        post_send(content.decode("utf-8"))
-                    except Exception:
-                        cli = False
-            except Exception as error:
-                break
-        client.settimeout(None)
-        client.close()
+                data = q.get(timeout = 0.001)
+            except Exception:
+                uslovie = False
+            if data:
+                public_key_socket.get(data[64:64]).send(data.encode())
+        #ОШИБКИ
+        for s in exceptional:
+            inputs.remove(s)
+            if s in outputs:
+                outputs.remove(s)
+            s.close()
+            del message_queues[s]
+
+
 
 def serv(q):
     MyServer("0.0.0.0",5000, q).serve_forever()
