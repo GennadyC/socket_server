3f4bf5ef367cb4cb2a2e375f670e7e50a43e9569
GennadyC <gena_chursin1995@mail.ru>
2019-03-27 18:59:31 +0700

    Update start_server.py

--- start_server.py
+++ start_server.py
@@ -5,32 +5,42 @@ from multiprocessing import Process, Queue
 import select
 import sys
 import requests
-import logging
 import json
 import queue
+import logging
 
 
 q=Queue()
 
 def post_send(msg):
     try:
-        #res = requests.post('http://127.0.0.1:8200/api/services/device_key_service/rebroadcast', json={"tx_body":msg})
-        res = requests.post('http://192.168.88.112:8200/api/services/device_key_service/rebroadcast', json={"tx_body":msg})
-    
+        res = requests.post('http://127.0.0.1:8200/api/services/device_key_service/rebroadcast', json={"tx_body":msg})
+        return "Ok"
+        logging.info(b'POST short_key Ok')
     except Exception:
-        pass 
+        logging.info(b'Client close')
+        return "Close" 
 
 def post_send_subscribe(msg):
-    res = requests.post('http://127.0.0.1:8100/api/services/device_key_service/subscribe', json={"tx_body":msg})
-
-def get_wearout():
-    res = requests.get('http://127.0.0.1:8200/api/services/device_key_service/get_short_device_key?public_key=91fc61fc1ed1856da51725f530951917ab5301d1c1f72a88a86c2950cb8f44ff')
-    if res.ok:
-        json_data = json.loads(res.text)
-        s = json_data.get('wearout')
-        return s
-    else:
-        return 1111111
+    try:
+        res = requests.post('http://127.0.0.1:8100/api/services/device_key_service/subscribe', json={"tx_body":msg})
+        return "Ok"
+    except Exception:
+        logging.info(b'Client close')
+        return "Close"
+        
+def get_wearout(msg):
+    try:
+        res = requests.get('http://127.0.0.1:8200/api/services/device_key_service/get_short_device_key?public_key='+msg)
+        if res.ok:
+            json_data = json.loads(res.text)
+            s = json_data.get('wearout')
+            return s
+        else:
+            return 1111111
+    except Exception:
+        logging.info(b'Client close')
+        return "Close"
 
 
 class MyServer(ApiServer):
@@ -40,6 +50,7 @@ class MyServer(ApiServer):
     @ApiRoute("/")
     def addbar(req):
         q.put(str(req["tx_body"]))
+        logging.info(b'Post from Exonum')
         #return {"tx_body":req["tx_body"]+1}
 
     @ApiRoute("/baz")
@@ -54,39 +65,77 @@ def sock(q):
     server.setblocking(0)
     server.listen(1024)
     addr = False
-    inputs = [server] #Сокеты которые планируем читать
-    outputs = []      #Сокеты в которые планируем писать
+    inputs = [server] 
+    outputs = []      
     message_queues = {}
     public_key_socket = {}
 
     while inputs:
         readable, writable, exceptional = select.select (inputs, outputs, inputs)
-        #ЧТЕНИЕ
+
         for s in readable:
             if s is server:
                 connection, client_address = s.accept()
-                print(client_address)
+                logging.info(client_address)
                 connection.setblocking(0)
                 inputs.append(connection)
                 outputs.append(connection)
                 message_queues[connection] = queue.Queue()
 
             else:
-                data = s.recv(1024)
+                try:
+                    data_encode = s.recv(1024)
+                    data = data_encode.decode()
+                except Exception:
+                    data = ""
                 if data:
                     if data[0:7] == "404Code":
-                        post_send_subscribe(data[7:])
-                        public_key_socket[data[7:64]] = s
-                        s.send(get_wearout(data[7:64]))
+                        logging.info(b'Message from client with 404Code')
+                        r = post_send_subscribe(data[7:])
+                        if r == "Close":
+                            try:
+                                inputs.remove(s)
+                                if s in outputs:
+                                    outputs.remove(s)
+                                s.close()
+                                del message_queues[s]
+                                del public_key_socket[data[7:71]]
+                            except Exception:
+                                pass
+                        public_key_socket[data[7:71]] = s
+                        wearout = get_wearout(data[7:71])
+                        if wearout == "Close":
+                            try:
+                                inputs.remove(s)
+                                if s in outputs:
+                                    outputs.remove(s)
+                                s.close()
+                                del message_queues[s]
+                                del public_key_socket[data[7:71]]
+                            except Exception:
+                                pass
+                            
+                        else:
+                            s.send(str(wearout).encode())
                     if data[0:7] == "410Code":
-                        post_send(data[7:])
+                        logging.info(b'Message from client with 410Code')
+                        p = post_send(data[7:])
+                        if p == "Close":
+                            try:
+                                inputs.remove(s)
+                                if s in outputs:
+                                    outputs.remove(s)
+                                s.close()
+                                del message_queues[s]
+                                del public_key_socket[data[64:128]]
+                            except Exception:
+                                pass
                 else:
                     if s in outputs:
                         outputs.remove(s)
                     inputs.remove(s)
                     s.close()
                     del message_queues[s]
-        #ЗАПИСЬ
         uslovie = True
         while uslovie:
             data = ""
@@ -95,8 +144,10 @@ def sock(q):
             except Exception:
                 uslovie = False
             if data:
-                public_key_socket.get(data[64:64]).send(data.encode())
-        #ОШИБКИ
+                try:
+                    public_key_socket[data[64:128]].send(data.encode())
+                except Exception:
+                    pass
         for s in exceptional:
             inputs.remove(s)
             if s in outputs:
@@ -110,10 +161,10 @@ def serv(q):
     MyServer("0.0.0.0",5000, q).serve_forever()
 
 def main():
+    logging.basicConfig(format = u'%(levelname)-8s [%(asctime)s] %(message)s', level = logging.INFO, filename = u'SocketLog.log')
 
     s = Process(target=serv, args=(q,))
     a = Process(target=sock, args=(q,))
-
     s.start()
     a.start()
     s.join()
